6fe42b4ff3b9884a681c237e40d7c898a4e8e888
nyefan <git@nyefan.org>
2026-07-06 12:40:00 -0700

    chore: organize by question number

--- leetcode_practice/src/lib.rs
+++ leetcode_practice/src/lib.rs
@@ -1,6 +1,6 @@
-pub mod addtwonumbers;
-pub mod countandsay;
-pub mod lrucache;
-pub mod numberofsubstringscontainingallthreecharacters;
-pub mod oddevenlinkedlist;
-pub mod palindromepartitioning;
+pub mod q00002_add_two_numbers;
+pub mod q00038_count_and_say;
+pub mod q00131_palindrome_partitioning;
+pub mod q00146_lru_cache;
+pub mod q00328_odd_even_linked_list;
+pub mod q01358_number_of_substrings_containing_all_three_characters;
